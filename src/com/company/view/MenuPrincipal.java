5617a90e5e27d231bfbe0bcb7c7579e6efab717f
Thetyzon <dalvarezr95@elpuig.xeill.net>
2019-05-13 17:08:59 +0200

    prueba

--- src/com/company/view/MenuPrincipal.java
+++ src/com/company/view/MenuPrincipal.java
@@ -1,4 +1,18 @@
 package com.company.view;
 
 public class MenuPrincipal {
+    public static void main(String[] args) {
+
+
+        switch (true) {
+            case 1:
+                System.out.println("hola");
+                break;
+            case 2:
+                // code block
+                break;
+            default:
+                // code block
+        }
+    }
 }
