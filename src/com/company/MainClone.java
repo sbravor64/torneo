b767f257a328573792f87fda368ac04f5e2342d4
Pratik <ppatel77@elpuig.xeill.net>
2019-05-13 17:42:55 +0200

    nuevo proyecto

--- src/com/company/MainClone.java
+++ src/com/company/MainClone.java
@@ -2,21 +2,40 @@ package com.company;
 
 import com.company.model.Equipo;
 import com.company.model.Participante;
-import java.io.*;
 
-public class Main {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainClone {
 
     public static void main(String[] args) throws IOException {
 
         Files file = new Files();
         Equipo equipo = new Equipo();
+        Participante participante = new Participante();
+
 
         file.ruta="equipo";
         file.SEPARATOR=";";
 
         File fileEquipo = new File(file.ruta);
 
+        equipo.nombre = "Barcelona";
+        equipo.siglas = "FCB";
+        equipo.ciudad = "Catalunya";
+
         BufferedWriter ficherosalida = new BufferedWriter(new FileWriter(fileEquipo, true));
+
+        ficherosalida.write(equipo.nombre + file.SEPARATOR + equipo.ciudad + file.SEPARATOR +equipo.siglas);
+        ficherosalida.close();
+
+
+        //
+        equipo.nombre = "Real Madrid";
+        equipo.siglas = "RMCF";
+        equipo.ciudad = "Madrid";
 
         ficherosalida.write(equipo.nombre + file.SEPARATOR + equipo.ciudad + file.SEPARATOR +equipo.siglas);
         ficherosalida.close();
--- src/com/company/manager/InscribirEquipo.java
+++ src/com/company/manager/InscribirEquipo.java
@@ -1,12 +1,15 @@
-package com.company.model;
+package com.company.manager;
 
+import javax.security.sasl.SaslClient;
 import java.util.Scanner;
 
-public class Equipo {
+public class InscribirEquipo {
     Scanner teclado = new Scanner(System.in);
 
-
     public String nombre=teclado.nextLine();
-    public String  siglas=teclado.nextLine();
     public String ciudad=teclado.nextLine();
+    public String  siglas=teclado.nextLine();
+
+
+
 }
