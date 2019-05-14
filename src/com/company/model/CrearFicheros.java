8d488daa991217cb14c24833510c1144dff8a1c2
Pratik <ppatel77@elpuig.xeill.net>
2019-05-14 16:32:14 +0200

    nuevo proyecto

---
+++ src/com/company/model/CrearFicheros.java
@@ -0,0 +1,13 @@
+package com.company.model;
+
+import java.io.File;
+
+ public class CrearFicheros {
+
+     public void newsFiles(){
+         Files file = new Files();
+         file.crearFicheros("equipo");
+         file.crearFicheros("Participantes");
+     }
+
+ }
