80683dfd17c0190f3577bad9d29bdfec0a9812cb
Thetyzon <dalvarezr95@elpuig.xeill.net>
2019-05-14 16:36:40 +0200

    Clases equipo creadas

--- src/com/company/view/MenuEquipo.java
+++ src/com/company/view/MenuEquipo.java
@@ -2,12 +2,20 @@ package com.company.view;
 
 import java.util.Scanner;
 import com.company.manager.InscribirEquipo;
+import com.company.manager.ModificarEquipo;
+import com.company.manager.BorrarEquipo;
+import com.company.manager.ListarEquipo;
+
 
 
 public class MenuEquipo {
     void show() {
         Scanner sc = new Scanner(System.in);
         InscribirEquipo inscribirEquipo = new InscribirEquipo();
+        ModificarEquipo modificarEquipo = new ModificarEquipo();
+        BorrarEquipo borrarEquipo = new BorrarEquipo();
+        ListarEquipo listarEquipo = new ListarEquipo();
+
 
         System.out.println("1. Equipo");
         System.out.println("    1. Inscribir");
@@ -19,16 +27,16 @@ public class MenuEquipo {
         int opcion = sc.nextInt();
         switch (opcion) {
             case 1:
-                inscribirEquipo
+                inscribirEquipo.inscribir();
                 break;
             case 2:
-                System.out.println("holae2");
+                inscribirEquipo.inscribir();
                 break;
             case 3:
-                System.out.println("holae3");
+                inscribirEquipo.inscribir();
                 break;
             case 4:
-                System.out.println("holae4");
+                inscribirEquipo.inscribir();
                 break;
             case 5:
                 break;
