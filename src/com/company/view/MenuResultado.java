ef5625bc2a95f09fa782f38f02fd1de2641e03e7
Pratik <ppatel77@elpuig.xeill.net>
2019-05-21 17:58:12 +0200

    nuevo proyecto

--- src/com/company/view/MenuResultado.java
+++ src/com/company/view/MenuResultado.java
@@ -9,18 +9,33 @@ public class MenuResultado {
     void show() throws IOException {
         Scanner sc = new Scanner(System.in);
         Introducir introducir = new Introducir();
-        System.out.println("3. Resultado");
-        System.out.println("    1. Introducir resultado");
-        System.out.println("    2. Salir");
 
-        int opcion = sc.nextInt();
-        switch (opcion) {
-            case 1:
-                introducir.introducirResultado();
-                break;
-            case 2:
-                break;
+        while(true) {
+            System.out.println("\033[92m3. Resultado\033[0m");
+            System.out.println("    1. Introducir resultado");
+            System.out.println("    2. Salir");
+
+            int opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1:
+                    System.out.print("IDJugador1: ");
+                    String idjugador1 = sc.nextLine();
+                    System.out.print("PuntosJ1: ");
+                    String puntosj1 = sc.nextLine();
+                    System.out.print("IDJugador2: ");
+                    String idjugador2 = sc.nextLine();
+                    System.out.print("PuntosJ2: ");
+                    String puntosj2 = sc.nextLine();
+
+                    introducir.introducirResultado(idjugador1, puntosj1, idjugador2, puntosj2);
+                    break;
+                case 2:
+                    return;
+            }
         }
+
+
 
     }
 }
